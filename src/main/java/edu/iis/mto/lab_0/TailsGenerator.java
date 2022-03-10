9e82019f18e32a20483a3f1fa222a023a20ff689
nowak.piotr <217728@edu.p.lodz.pl>
2022-03-10 17:46:34 +0100

    lab 0 commit
    commit realizujący lab 0

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,12 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < value.length(); i++) {
+            list.add(value.substring(i));
+        }
+        list.add("");
+        return list;
     }
 
 }
